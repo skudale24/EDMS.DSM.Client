72427458a316b244fe8579c86ce6cab8b893e95e
siddharth.k <siddharth.k@iworktech.com>
2023-06-05 21:56:23 +0530

    Remove unnecessary files

--- Client/Managers/Common/IUploadManager.cs
+++ Client/Managers/Common/IUploadManager.cs
@@ -3,11 +3,8 @@
 public interface IUploadManager : IManager
 {
     Task<IApiResult> UploadFileAsync(string uploadUri, MemoryStream ms, string fileName);
-    Task<IListApiResult<List<CommonUploadDto>>> SearchAsync(CommonUploadFilter commonUploadFilter, int orgId);
-    Task<IListApiResult<List<CommonUploadDto>>> GetUploadsAsync(int orgId);
     Task<IApiResult> DeleteAsync(int id);
     Task<IApiResult> TransferDataAsync(int id);
-    Task<Stream> DownloadErrorFileAsync(string CSVFileName);
     Task<Stream> DownloadSourceFileAsync(string fileName);
     Task<Stream> DownloadExcelFileAsync<TIn>(TIn communication);
     Task<Stream> ExportGridAsync<TIn>(TIn communications, List<GridColumnDTO> gridColumns);
